ccf8452dcc8d8ea1f38250332702ffd1d9db3517
Madeleine Woodbury <randine25@gmail.com>
2019-10-19 16:21:45 -0500

    Set up the server

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Api running');
+});
+
+app.listen(3001, () => console.log('Server running on port 3001'));
