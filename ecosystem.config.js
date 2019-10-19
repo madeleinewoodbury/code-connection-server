02672ee8e9f456d1ae584b37a8357d8af686a7f2
Madeleine Woodbury <randine25@gmail.com>
2019-10-19 16:55:41 -0500

    PM2 Setup

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'code-connection-server',
+      script: './index.js'
+    }
+  ],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-216-232-225.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/code-connection.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:madeleinewoodbury/code-connection-server.git',
+      path: '/home/ubuntu/code-connection',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+};
